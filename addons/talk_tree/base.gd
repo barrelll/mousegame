91afb0728aa939a8a684a26ec43a651b4692688e
barrelll <jharrell_@outlook.com>
2024-10-03 21:18:23 -0500

    added a tree!

--- addons/talk_tree/base.gd
+++ addons/talk_tree/base.gd
@@ -1,1 +1,23 @@
+@tool
 extends HBoxContainer
+
+@onready var tree: Tree = $Panel/TabContainer/Characters
+var root: TreeItem
+
+
+func _ready() -> void:
+	root = tree.create_item()
+	var one = tree.create_item(root)
+	one.set_text(0, "First Blind Mouse")
+	var child = one.create_child()
+	child.set_text(0, "Text!")
+
+
+func _on_tree_cell_selected() -> void:
+	print("dsada")
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_text_caret_word_right.macos"):
+		$Panel.hide()
+	elif event.is_action_pressed("ui_text_caret_word_left.macos"):
+		$Panel.show()
